3a04e09222f8cfd18fb4aa6b70f7d635cc3a6093
tommy.tang <tommy.tang@dianping.com>
2016-04-18 19:56:06 +0800

    Initial Codes

---
+++ anole-client/anole-subscriber-client/src/main/java/org/anole/subscriber/client/App.java
@@ -0,0 +1,13 @@
+package org.anole.subscriber.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ anole-server/anole-hub/src/main/java/org/anole/hub/App.java
@@ -0,0 +1,13 @@
+package org.anole.hub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
